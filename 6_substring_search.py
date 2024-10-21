a92317de68529bdc1cf9a89b9f5ddd76527a7972
SovitT <sovitthakre1313@gmail.com>
2024-10-21 17:47:41 +0530

    Update 6_substring_search.py

--- 6_substring_search.py
+++ 6_substring_search.py
@@ -1,21 +1,8 @@
 # Exercise 6: Substring Search
-
-numbers = []
-for i in range(5):
-    while True:
-        try:
-            num = float(input(f"Enter number {i+1}: "))
-            numbers.append(num)
-            break
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-
-sum_of_numbers = sum(numbers)
-print(f"\nThe list of numbers: {numbers}")
-print(f"The sum of all numbers in the list: {sum_of_numbers}")
-
-
-largest_number = max(numbers)
-smallest_number = min(numbers)
-print(f"The largest number in the list: {largest_number}")
-print(f"The smallest number in the list: {smallest_number}")
+sentence = input("Please enter a sentence: ")
+word_to_search = input("Please enter a word to search for: ")
+if word_to_search in sentence:
+    position = sentence.index(word_to_search)
+    print(f"The word '{word_to_search}' exists in the sentence at index {position}.")
+else:
+    print(f"The word '{word_to_search}' does not exist in the sentence.")
